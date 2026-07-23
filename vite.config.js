29342db1c65a45fcb21269c2e7ae815a32c55f6d
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-23 17:30:33 -0400

    Remove missing quote entry from main Vite build

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,6 @@ export default defineConfig({
         portfolio: resolve(__dirname, 'portfolio.html'),
         about: resolve(__dirname, 'about.html'),
         contact: resolve(__dirname, 'contact.html'),
-        quote: resolve(__dirname, 'quote.html'),
       },
     },
   },
