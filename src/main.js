aebc47eaa6468bea3c5b1f05c68889ad1c81f068
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 17:04:49 -0400

    Load complete homepage work slideshow

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import './service-layout-tweaks.css';
 import './reviews.css';
 import { renderPage } from './site.js';
 import { renderPublishedReviews } from './reviews.js';
+import { initializeCompleteWorkSlideshow } from './home-work-slideshow.js';
 
 renderPage('home');
+initializeCompleteWorkSlideshow();
 renderPublishedReviews();
