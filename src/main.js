f76f1d066459d9cccaddd92d9223cc073da1c895
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 01:01:06 -0400

    Load approved reviews on homepage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import './style.css';
 import './alpha-polish.css';
+import './reviews.css';
 import { renderPage } from './site.js';
+import { renderPublishedReviews } from './reviews.js';
 
 renderPage('home');
+renderPublishedReviews();
