436139e78834278873d435a1616eebcac39d8b34
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 14:26:29 -0400

    Load final public layout spacing

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,7 @@
 import './style.css';
 import './alpha-polish.css';
 import './layout-repair.css';
+import './service-layout-tweaks.css';
 import { renderPage } from './site.js';
 
 renderPage('portfolio');
