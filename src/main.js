47434e110d32844bfca9b595f02afd9223851b35
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 14:26:05 -0400

    Load final public layout spacing

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './style.css';
 import './alpha-polish.css';
 import './layout-repair.css';
+import './service-layout-tweaks.css';
 import './reviews.css';
 import { renderPage } from './site.js';
 import { renderPublishedReviews } from './reviews.js';
