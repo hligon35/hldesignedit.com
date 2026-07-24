573773e4a2e5384131d54814909788d8fab30b62
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 16:09:05 -0400

    Load six-column work gallery styles

--- src/portfolio.js
+++ src/portfolio.js
@@ -2,6 +2,7 @@ import './style.css';
 import './alpha-polish.css';
 import './layout-repair.css';
 import './service-layout-tweaks.css';
+import './photo-gallery-grid.css';
 import { renderPage } from './site.js';
 
 renderPage('portfolio');
