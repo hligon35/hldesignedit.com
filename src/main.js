84605da75d1e65d0170a7a2a07af1fdbaf41a88c
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-24 01:48:43 -0400

    Load shared layout repairs on homepage

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './style.css';
 import './alpha-polish.css';
+import './layout-repair.css';
 import './reviews.css';
 import { renderPage } from './site.js';
 import { renderPublishedReviews } from './reviews.js';
